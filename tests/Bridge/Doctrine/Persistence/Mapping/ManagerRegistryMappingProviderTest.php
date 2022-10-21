a1c8856d52806e9772ad71d8215f33d8441d593f
Kevin Bond <kevinbond@gmail.com>
2022-10-21 14:42:02 -0400

    add CacheMappingProvider

--- tests/Bridge/Doctrine/Persistence/Mapping/ManagerRegistryMappingProviderTest.php
+++ tests/Bridge/Doctrine/Persistence/Mapping/ManagerRegistryMappingProviderTest.php
@@ -3,56 +3,16 @@
 namespace Zenstruck\Document\Library\Tests\Bridge\Doctrine\Persistence\Mapping;
 
 use Zenstruck\Document\Bridge\Doctrine\Persistence\Mapping\ManagerRegistryMappingProvider;
-use Zenstruck\Document\Library\Tests\Bridge\Doctrine\Fixture\Entity1;
-use Zenstruck\Document\Library\Tests\Bridge\Doctrine\HasORM;
-use Zenstruck\Document\Library\Tests\TestCase;
+use Zenstruck\Document\Bridge\Doctrine\Persistence\MappingProvider;
+use Zenstruck\Document\Library\Tests\Bridge\Doctrine\Persistence\MappingProviderTest;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-final class ManagerRegistryMappingProviderTest extends TestCase
+final class ManagerRegistryMappingProviderTest extends MappingProviderTest
 {
-    use HasORM;
-
-    /**
-     * @test
-     */
-    public function can_get_mapping_for_class(): void
+    protected function provider(): MappingProvider
     {
-        $provider = new ManagerRegistryMappingProvider($this->doctrine());
-
-        $this->assertSame(
-            [
-                'document1' => [
-                    'library' => 'memory',
-                ],
-                'document2' => [
-                    'library' => 'public',
-                ],
-            ],
-            $provider->get(Entity1::class)
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function can_get_all_mappings(): void
-    {
-        $provider = new ManagerRegistryMappingProvider($this->doctrine());
-
-        $this->assertSame(
-            [
-                Entity1::class => [
-                    'document1' => [
-                        'library' => 'memory',
-                    ],
-                    'document2' => [
-                        'library' => 'public',
-                    ],
-                ],
-            ],
-            $provider->all()
-        );
+        return new ManagerRegistryMappingProvider($this->doctrine());
     }
 }
---
+++ tests/Bridge/Doctrine/Persistence/MappingProviderTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Zenstruck\Document\Library\Tests\Bridge\Doctrine\Persistence;
+
+use Zenstruck\Document\Bridge\Doctrine\Persistence\MappingProvider;
+use Zenstruck\Document\Library\Tests\Bridge\Doctrine\Fixture\Entity1;
+use Zenstruck\Document\Library\Tests\Bridge\Doctrine\HasORM;
+use Zenstruck\Document\Library\Tests\TestCase;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+abstract class MappingProviderTest extends TestCase
+{
+    use HasORM;
+
+    /**
+     * @test
+     */
+    public function can_get_mapping_for_class(): void
+    {
+        $this->assertSame(
+            [
+                'document1' => [
+                    'library' => 'memory',
+                ],
+                'document2' => [
+                    'library' => 'public',
+                ],
+            ],
+            $this->provider()->get(Entity1::class)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function can_get_all_mappings(): void
+    {
+        $this->assertSame(
+            [
+                Entity1::class => [
+                    'document1' => [
+                        'library' => 'memory',
+                    ],
+                    'document2' => [
+                        'library' => 'public',
+                    ],
+                ],
+            ],
+            $this->provider()->all()
+        );
+    }
+
+    abstract protected function provider(): MappingProvider;
+}
