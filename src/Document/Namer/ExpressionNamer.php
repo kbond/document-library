683eab27f0102fd37c036d60f60ed7556e4b6780
Kevin Bond <kevinbond@gmail.com>
2022-10-25 14:47:07 -0400

    allow adjusting checksum/rand expressions

--- src/Document/Namer/ExpressionNamer.php
+++ src/Document/Namer/ExpressionNamer.php
@@ -18,17 +18,30 @@ final class ExpressionNamer extends BaseNamer
     public function generateName(Document $document, array $context = []): string
     {
         return \preg_replace_callback(
-            '#{(name|ext|checksum|rand)}#',
-            function($matches) use ($document) {
-                return match ($matches[0]) {
-                    '{name}' => $this->slugify($document->nameWithoutExtension()),
-                    '{ext}' => self::extensionWithDot($document),
-                    '{checksum}' => $document->checksum(),
-                    '{rand}' => self::randomString(),
-                    default => throw new \LogicException('Invalid match.'),
+            '#{([\w.:\-]+)}#',
+            function($matches) use ($document, $context) {
+                return match ($matches[1]) {
+                    'name' => $this->slugify($document->nameWithoutExtension()),
+                    'ext' => self::extensionWithDot($document),
+                    'checksum' => $document->checksum(),
+                    'rand' => self::randomString(),
+                    default => self::parseVariable($matches[1], $document, $context),
                 };
             },
             $context['expression'] ?? $this->defaultExpression
         );
+    }
+
+    private static function parseVariable(string $variable, Document $document, array $context): string
+    {
+        if (2 === \count($parts = \explode(':', $variable))) {
+            return match (\mb_strtolower($parts[0])) {
+                'checksum' => \mb_substr($document->checksum(), 0, (int) $parts[1]),
+                'rand' => self::randomString((int) $parts[1]),
+                default => throw new \LogicException(\sprintf('Unable to parse expression variable {%s}.', $variable)),
+            };
+        }
+
+        throw new \LogicException(\sprintf('Unable to parse expression variable {%s}.', $variable));
     }
 }
--- tests/Namer/ExpressionNamerTest.php
+++ tests/Namer/ExpressionNamerTest.php
@@ -48,6 +48,10 @@ final class ExpressionNamerTest extends TestCase
             'a/prefix/path--9a0364b9e99bb480dd25e1f0284c8555.txt',
             $namer->generateName($document, ['expression' => 'a/prefix/{name}--{checksum}{ext}'])
         );
+        $this->assertSame(
+            'a/prefix/path--9a0364b.txt',
+            $namer->generateName($document, ['expression' => 'a/prefix/{name}--{checksum:7}{ext}'])
+        );
     }
 
     /**
@@ -65,5 +69,36 @@ final class ExpressionNamerTest extends TestCase
         $this->assertMatchesRegularExpression('#^[0-9a-z]{6}-[0-9a-z]{6}$#', $name2);
         $this->assertNotSame($name1, $name2);
         $this->assertSame(13, \mb_strlen($name1));
+    }
+
+    /**
+     * @test
+     */
+    public function can_customize_rand_length(): void
+    {
+        $namer = new ExpressionNamer();
+        $document = self::inMemoryLibrary()->store('some/pATh.txt', 'content');
+
+        $name1 = $namer->generateName($document, ['expression' => '{rand:3}-{rand:10}']);
+        $name2 = $namer->generateName($document, ['expression' => '{rand:3}-{rand:10}']);
+
+        $this->assertMatchesRegularExpression('#^[0-9a-z]{3}-[0-9a-z]{10}$#', $name1);
+        $this->assertMatchesRegularExpression('#^[0-9a-z]{3}-[0-9a-z]{10}$#', $name2);
+        $this->assertNotSame($name1, $name2);
+        $this->assertSame(14, \mb_strlen($name1));
+    }
+
+    /**
+     * @test
+     */
+    public function invalid_expression_variable(): void
+    {
+        $namer = new ExpressionNamer();
+        $document = self::inMemoryLibrary()->store('some/pATh.txt', 'content');
+
+        $this->expectException(\LogicException::class);
+        $this->expectDeprecationMessage('Unable to parse expression variable {invalid}.');
+
+        $namer->generateName($document, ['expression' => 'prefix/{invalid}']);
     }
 }
