d47d52773cb32776313651f53dd326e87755ac76
Jakub Caban <kuba.iluvatar@gmail.com>
2022-12-14 10:46:57 +0100

    Handle ValueResolverInterface deprecation

--- src/Document/Library/Bridge/Symfony/HttpKernel/PendingDocumentValueResolver.php
+++ src/Document/Library/Bridge/Symfony/HttpKernel/PendingDocumentValueResolver.php
@@ -4,6 +4,7 @@ namespace Zenstruck\Document\Library\Bridge\Symfony\HttpKernel;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Zenstruck\Document\Attribute\UploadedFile;
 use Zenstruck\Document\PendingDocument;
@@ -13,42 +14,74 @@ use Zenstruck\Document\PendingDocument;
  *
  * @internal
  */
-class PendingDocumentValueResolver implements ArgumentValueResolverInterface
-{
-    public function __construct(
-        private RequestFilesExtractor $filesExtractor
-    ) {
-    }
-
-    public function supports(Request $request, ArgumentMetadata $argument): bool
+if (interface_exists(ValueResolverInterface::class)) {
+    class PendingDocumentValueResolver implements ValueResolverInterface
     {
-        return PendingDocument::class === $argument->getType()
-            || !empty($argument->getAttributes(UploadedFile::class));
-    }
-
-    /**
-     * @return iterable<PendingDocument|array|null>
-     */
-    public function resolve(Request $request, ArgumentMetadata $argument): iterable
-    {
-        $attributes = $argument->getAttributes(UploadedFile::class);
-
-        if (
-            empty($attributes)
-            && PendingDocument::class !== $argument->getType()
-        ) {
-            return [];
+        public function __construct(
+            private RequestFilesExtractor $filesExtractor
+        )
+        {
         }
 
-        $path = $attributes[0]?->path
-            ?? $argument->getName();
+        /**
+         * @return iterable<PendingDocument|array|null>
+         */
+        public function resolve(Request $request, ArgumentMetadata $argument): iterable
+        {
+            $attributes = $argument->getAttributes(UploadedFile::class);
 
-        return [
-            $this->filesExtractor->extractFilesFromRequest(
-                $request,
-                $path,
-                PendingDocument::class !== $argument->getType()
-            ),
-        ];
+            if (
+                empty($attributes)
+                && PendingDocument::class !== $argument->getType()
+            ) {
+                return [];
+            }
+
+            $path = $attributes[0]?->path
+                ?? $argument->getName();
+
+            return [
+                $this->filesExtractor->extractFilesFromRequest(
+                    $request,
+                    $path,
+                    PendingDocument::class !== $argument->getType()
+                ),
+            ];
+        }
+    }
+} else {
+    class PendingDocumentValueResolver implements ArgumentValueResolverInterface
+    {
+        public function __construct(
+            private RequestFilesExtractor $filesExtractor
+        )
+        {
+        }
+
+        public function supports(Request $request, ArgumentMetadata $argument): bool
+        {
+            return PendingDocument::class === $argument->getType()
+                || !empty($argument->getAttributes(UploadedFile::class));
+        }
+
+        /**
+         * @return iterable<PendingDocument|array|null>
+         */
+        public function resolve(Request $request, ArgumentMetadata $argument): iterable
+        {
+            $attributes = $argument->getAttributes(UploadedFile::class);
+            \assert(!empty($attributes));
+
+            $path = $attributes[0]?->path
+                ?? $argument->getName();
+
+            return [
+                $this->filesExtractor->extractFilesFromRequest(
+                    $request,
+                    $path,
+                    PendingDocument::class !== $argument->getType()
+                ),
+            ];
+        }
     }
 }
